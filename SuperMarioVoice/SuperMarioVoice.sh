02f1b6207ea4c65b7e356069be001ce9330fdf14
Sergio Benlloch <36139340+sg1o@users.noreply.github.com>
2021-05-03 15:54:15 +0200

    Update SuperMarioVoice.sh

--- SuperMarioVoice/SuperMarioVoice.sh
+++ SuperMarioVoice/SuperMarioVoice.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-
-if [[ $EUID -ne 0 ]]; then
-    echo "El script se tiene que ejecutar como sudo" 
-    exit 1
-fi
-
 printf "========================================================================\n"
 printf '==============      Ejecución de Super Mario Voice       ===============\n'
 printf "========================================================================\n"
@@ -13,7 +6,7 @@ SCRIPT=$(readlink -f $0);
 dir_base=`dirname $SCRIPT`;
 
 if [ "$#" -eq 0 ];then
-    sudo -u $USER padsp snes9x $dir_base/rom/rom.sfc &>/dev/null
+    padsp snes9x $dir_base/rom/rom.sfc &>/dev/null
     sudo python3 asistenteSMV.py
     else
         sudo -u $USER padsp snes9x $dir_base/rom/rom.sfc &>/dev/null
